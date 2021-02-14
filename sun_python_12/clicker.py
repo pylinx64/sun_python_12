e5a7cea489b1c6ebd6a9cbdd16a00b52c5f0c82b
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-14 13:17:37 +0200

    Add files via upload

---
+++ sun_python_12/clicker.py
@@ -0,0 +1,8 @@
+import pyautogui
+
+pyautogui.click(500, 700, duration=1)
+pyautogui.click(680, 590,  duration=1)
+#pyautogui.PAUSE = 5
+while True:
+	pyautogui.typewrite('MINECRAFT TOP')
+	pyautogui.keyDown('Enter')
