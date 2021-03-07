1559c28924755d20c04785e325cc7324bab82581
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-07 20:05:51 +0200

    Add files via upload

--- sun_python_12/client.py
+++ sun_python_12/client.py
@@ -12,7 +12,7 @@ def receving (name, sock, switch):
 			while True:
 				data, addr = sock.recvfrom(1024)
 				print('\n'+data.decode("utf-8"))
-                time.sleep(0.2)	
+				time.sleep(0.2)	
 		except:
 			pass
 
@@ -21,4 +21,27 @@ def receving (name, sock, switch):
 shutdown = False
 join = False
 
+# ваш ip, ваш порт
+host = socket.gethostbyname(socket.gethostname())
+port = 0
 
+# сервер ip, сервер порт
+server = ("192.168.1.132", 11719)
+
+# создает сокет
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind((host,port))
+s.setblocking(0)
+
+name = input("$ name: ")
+# отправляет сообщение на сервер
+s.sendto(("["+name+"] => join chat ").encode("utf-8"), server)
+time.sleep(0.2)
+
+# собирает сообщения с сервера
+rT = threading.Thread(target = receving, args = ("RecvThread", s, shutdown))
+rT.start()
+
+# закрывает соединение (для безопасности)
+rT.join()
+s.close()
