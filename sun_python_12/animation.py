06fc75045339bb11c7bf955b7e4224f14744e4d2
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-29 12:03:56 +0300

    Add files via upload

---
+++ sun_python_12/animation.py
@@ -0,0 +1,19 @@
+import pygame
+
+# запуск pygame
+pygame.init()
+
+# цвета RGB
+red = (255, 0, 0)
+black = (0, 0, 0)
+green = (0, 255, 0)
+white = (255, 255, 255)
+
+# экран
+size = [1024, 600]
+screen = pygame.display.set_mode(size)
+
+#-------------------------Основной цикл---------------------------------
+done=True
+while done==True:
+    pass
---
+++ sun_python_12/zadanie.py
@@ -0,0 +1,11 @@
+first_money = 100000
+p = 0.05
+money = 0
+
+for month in range(1, 13):
+    money = money + first_money * p
+
+
+print(money+first_money)
+
+
