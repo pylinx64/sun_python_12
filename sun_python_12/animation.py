6ee7da54060bcaeda12051105138b647846db93f
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-07 11:34:51 +0300

    Add files via upload

--- sun_python_12/animation.py
+++ sun_python_12/animation.py
@@ -14,6 +14,34 @@ size = [1024, 600]
 screen = pygame.display.set_mode(size)
 
 #-------------------------Основной цикл---------------------------------
+# FPS
+clock = pygame.time.Clock()
+
 done=True
 while done==True:
-    pass
+    # События 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done=False
+    # События конец
+    
+    
+    # Рисование
+    # экран, цвет, точка1, точка2, размер
+    pygame.draw.line(screen, white, [0, 0], [640, 360], 15)
+    # Рисование конец
+    
+    
+    # Логика
+    # Логика конец
+    
+    # обновление экрана
+    pygame.display.flip()
+    
+    # управление обновление экрана
+    clock.tick(60)
+    
+pygame.quit()
+
+    
+    
